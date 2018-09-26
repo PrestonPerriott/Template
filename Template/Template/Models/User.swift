d368b5c66e6c51b995b797e2b6003c6c8f7da969
Preston Perriott <perriott@Prestons-MacBook-Pro-2.local>
2018-09-26 14:52:27 -0400

    User Model Stub

---
+++ Template/Template/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Template
+//
+//  Created by Preston Perriott on 9/25/18.
+//  Copyright © 2018 Preston Perriott. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+///TODO: create backend to logically continue
+class User: Object, Codable {
+    
+}
+
